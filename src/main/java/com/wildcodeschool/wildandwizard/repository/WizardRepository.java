a206d3c730b413b239b1a123385b2264936eaaa5
Babongui mabika venuza <vhenuzya@yahoo.fr>
2022-11-02 13:33:25 +0100

    spring Ioc & D

--- src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
@@ -1,12 +1,15 @@
 package com.wildcodeschool.wildandwizard.repository;
 
 import com.wildcodeschool.wildandwizard.entity.Wizard;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
+@Repository
 public class WizardRepository implements WizardDao {
 
     private static List<Wizard> wizards = new ArrayList<>(
@@ -14,6 +17,7 @@ public class WizardRepository implements WizardDao {
                     new Wizard(1L, "Harry", "Potter", new Date(80, 6, 31), "London", "", false)
             )
     );
+
 
     @Override
     public Wizard save(Wizard wizard) {
